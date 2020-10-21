24971649377316384ed824317692397dc39bbbfe
pgm-timdesae <timdesae@student.ahs.be>
2020-10-21 16:01:14 +0200

    final

--- app/news.js
+++ app/news.js
@@ -3,69 +3,77 @@
 
 const posts = [
     {
-        title: 'title post 1',
-        synopsis: 'synopsis post 1',
-        body: 'Veeeeel tekst',
-        thumbnail: '',
+        title: 'TENTOONSTELLING ‘CONCERTFOTOGRAFIE’ DOOR GEERT BONNE',
+        synopsis: 'Geert, voormalig drummer bij Gorky, is een ‘gevoelsmens’. Zijn invalshoek bij het maken van de concertfoto’s, \n\t\t meer bepaald het sub-thema rockfotografie, is het gevoel van passie, geluk, concentratie en overgave dat \n \t\t muziekartiesten ervaren op een podium.',
+        body: '“Mijn voorkeur gaat daarbij uit naar zwart-wit foto’s omdat je meer de nadruk kan leggen op het onderwerp”, \n \t\t vertelt Geert. De foto’s zijn getrokken over een tijdsspanne van vijf jaar. In de tentoonstelling zien we onder \n \t\t andere Luc De Vos en Raymond van het Groenewoud passeren. Deze tentoonstelling moet studenten Grafische en \n \t\t Digitale Media inspireren en stimuleren om zelf hun creatieve blik te verruimen.',
+        thumbnail: 'https://dl.airtable.com/.attachments/ce0470435c01a2a03365cacb5f70def2/fc368b51/IMG_0521.JPG',
         created: new Date(1593512447000).toUTCString(),
         modified: new Date(1593512447000).toUTCString(),
         authors: [
         {
-            firstName: 'Tim',
-            lastName: 'De Saeger',
-            thumbnailURL: 'link van Tim'
-        },
-        {
-            firstName: 'Evi',
-            lastName: 'De Saeger',
-            thumbnailURL: 'link van Evi'
+            firstName: 'Lisa',
+            lastName: 'De Wilde',
+            thumbnailURL: 'https://dl.airtable.com/.attachments/8216307230f675f3fa6eb2aa90474c67/7ff726d6/lisa.jfif'
         }],
         comments: [
         {
-            nickName: 'sporza',
-            message: 'Belgie gaat nooit wereldkampioen worden',
+            nickName: 'Fenzo',
+            message: 'Waarom heeft Nederland hiervoor geen belangstelling? We komen niet voor in het rijtje van tien landen. Of zitten we bij die andere helft?',
             commentOnComment: [
             {   
-                nickName: 'Eden Hazard',
-                message: 'Pas toch maar op als ik terug ben kan alles'
+                nickName: 'Santford',
+                message: 'Nederland is een van de 32 deelnemende landen in de EuroHPC JU.'
+            },
+            {   
+                nickName: 'MartineEekhof',
+                message: 'Wij zetten grote windmolenparken op voor datacenters.',               
             }]
         },
         {
-            nickName: 'josje',
-            message: 'K3 was niets zonder mij',
+            nickName: '3raser',
+            message: 'AMD doet goede zaken op alle gebieden zo lijkt het. Fascinerend dat een grote speler als Intel momenteel een soort tweederangs lijkt te zijn.',
             commentOnComment: [
             {
-                nickName: 'Karen Daemen',
-                message: 'K3 is the best (the old one)'
+                nickName: 'FreVDP',
+                message: 'Intel is dan ook gestopt met innoveren omdat ze de x86 markt zo goed als alleen in handen hadden. Laten we van geluk spreken dat AMD weer helemaal mee is.'
             }]
         }]
     },
     {
-        title: 'title post 2',
-        synopsis: 'synopsis post 2',
-        body: 'Veeeeel tekst',
-        thumbnail: '',
+        title: 'RECAP: PROJECTWEEK CREATIVE SKILLS 21',
+        synopsis: '8 Landen, 8 scholen, 36 studenten, 16 docenten, 9 coördinatoren: de eerste werkweek van het project Creative Skills 21 \n\t\t (Erasmus+) ging vorige week in Gent van start. Het project heeft als doel docenten binnen creatieve opleidingen op \n\t\t te leiden om efficiënter 21st century skills over te brengen bij hun studenten. 21st century skills gaan over soft \n\t\t skills; niet de technische vaardigheden, maar zaken zoals creativiteit, samenwerking, communicatieve vaardigheden \n\t\t en oplossingsgericht denken.',
+        body: 'Zowel studenten als docenten volgden een workshop Human-Centered Design, waarbij op een praktische manier de principes \n\t\t van design thinking werden meegegeven. Problemen vinden en oplossen als een designer dus. De studenten zouden de rest \n\t\t van de week in groepen werken aan een eigen design challenge, waarbij ze gecoacht werden door de docenten. Iedere groep \n\t\t bestond uit 4 of 5 studenten en 2 docenten/coaches, elk uit een ander land. Ook werden de creative disciplines gemixt. \n\n\t\t Studenten en docenten met achtergronden in grafisch ontwerp, fotografie, videoproductie, animatie, interactieve media, \n\t\t app development, muziekproductie en zelfs interieurdesign zaten door elkaar. De challenge had te maken met een van de \n\t\t VN Sustainable Development Goals: "Inspire young people with meaningful behaviour change towards a sustainable lifestyle."',
+        thumbnail: 'https://dl.airtable.com/.attachments/1f0f9c5f37e20e0a5b42e7146c9d0a23/3ae0aa52/2019_11_29_day_05_DSF7405.jpg',
         created: new Date(1593512447000).toUTCString(),
         modified: new Date(1593512447000).toUTCString(),
         authors: [
         {
-            firstName: 'Franky',
-            lastName: 'Verelst',
-            thumbnailURL: 'link van Franky'
+            firstName: 'Sander',
+            lastName: 'Spek',
+            thumbnailURL: 'https://dl.airtable.com/.attachments/f39ebd0b2d7246d25f15082124f1824d/108870f5/sander.jfif'
         },
         {
-            firstName: 'Jef',
-            lastName: 'Van Achter Den Hoek',
-            thumbnailURL: 'link van Jef'
+            firstName: 'Philippe',
+            lastName: 'De Pauw',
+            thumbnailURL: 'https://dl.airtable.com/.attachments/b40f1495811869e4454052a7526164e9/852910af/CO84uTK8_400x400.jpg'
+        },
+        {
+            firstName: 'Lisa',
+            lastName: 'De Wilde',
+            thumbnailURL: 'https://dl.airtable.com/.attachments/8216307230f675f3fa6eb2aa90474c67/7ff726d6/lisa.jfif'
         }],
         comments: [
         {
-            nickName: 'fifaghost',
-            message: 'a long message',
+            nickName: 'Marsman 83',
+            message: 'Weet iemand waarom de sonde zich al zo lang in een baan om de planetoïde bevind (2018) voordat er geland werd?',
             commentOnComment: [
             {
-                message: 'You suck in fifa',
-                nickName: 'fifaghost',
+                message: "Zo'n landing is best gevaarlijk. Daarom hebben we eerst zo veel mogelijk data verzameld op een veilige afstand. Naast de \n\t  wetenschappelijke waarde van de data hebben ze ook veel geleerd over het navigeren rondom Bennu.",
+                nickName: 'AJediIAm',
+            },
+            {
+                message: 'In die tijd is er onderzoek gedaan vanuit de orbit, en gezocht naar landingsplaatsen.',
+                nickName: 'LOTG',
             }]
         },
         {
@@ -87,21 +95,21 @@ function showPostsInConsole(posts)
     let tempstr = '';
     posts.forEach((p, index) => {
     tempstr += 
-`=====================================================================================================================================
+`==========================================================================================================================================
 Post ${index + 1}: \t ${p.title} 
-=====================================================================================================================================
+==========================================================================================================================================
 synopsis: \t ${p.synopsis}
 body: \t\t ${p.body}
-thumbnail: \t ${p.thumbnailUrl}
+thumbnail: \t ${p.thumbnail}
 Created at: \t ${p.created}
 Modified at: \t ${p.modified}
-------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------
 AUTHORS
-------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------
 ${getStringForAuthors(p.authors)}
-------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------
 COMMENTS
-------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------
 ${getStringForComments(p.comments)}
 `
     });
@@ -113,8 +121,7 @@ ${getStringForComments(p.comments)}
 function getStringForAuthors (authors){
     let tempstr = '';
     authors.forEach((a, index) => {  
-        tempstr += `Author ${index + 1}: \t ${a.firstName} ${a.lastName} \n \t\t ${a.thumbnailUrl} 
-`;  
+        tempstr += `Author ${index + 1}: \t ${a.firstName} ${a.lastName} \n \t\t ${a.thumbnailURL} \n`;  
     });
     return tempstr;
 }
@@ -136,7 +143,8 @@ function getStringForComments (comments){
 function getStringForCommentOnComment (commentOnComment){
     let tempstr = '';
     commentOnComment.forEach((c, index) => {  
-        tempstr += `* ${c.message}  \n \t  written by:${c.nickName}`;  
+        tempstr += `* ${c.message}  \n \t  written by:${c.nickName}
+        ` ;  
     });
     return tempstr;
 }
@@ -144,8 +152,8 @@ function getStringForCommentOnComment (commentOnComment){
 
 // Create a message
 const mes = `
-=====================================================================================================================================
-|                                                            NEWS                                                                   |
+==========================================================================================================================================
+|                                                            NEWS                                                                        |
 ${showPostsInConsole(posts)}
 `
 
